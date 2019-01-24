4ca54f63fce424371c5a8d7f904d06a6467d2391
tahakkani <38796045+tahakkani@users.noreply.github.com>
2019-01-24 15:03:05 -0800

    Add files via upload

--- Assignment1/Test.cpp
+++ Assignment1/Test.cpp
@@ -1,5 +1,5 @@
-#include "QueueA.h"
-//#include "PQueue.h"
+#include "QueueA.cpp"
+#include "PQueue.cpp"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -7,26 +7,33 @@ using namespace std;
 
 int main()
 {
-	//PriorityQueue pQ;
+	PriorityQueue pQ;
 	Queue q;
+	Queue *qptr = &q;
 	//The following test assigns random
 	//integers between 0 and 99 to both
 	//queues and then proceeds to dequeue
 	//and display the front of each queue
 	//to see if they, in fact, were
-	//queued in the right order
+	//queued in the right order. It also
+	//tests if the queue is empty or full.
 	
 	srand((unsigned)time(0));
-	for(int i = 0; i < MAX_QUEUE; i++){
-		QueueItemType item = rand() % 50;
-	//	pQ.enqueue(item);
-		q.enqueue(item);
-	}
-	for(int i = 0; i < MAX_QUEUE; i++){
-		//cout << pQ.getFront() << "\t";
-		cout << q.getFront() << "\n";
-	//  pQ.dequeue();
-	  q.dequeue();
-	}
-	
+	int iter = 0; //tracks how many times the pointer changes
+	do{
+    for(int i = 0; i < MAX_QUEUE; i++){
+		  QueueItemType item = rand() % 100;
+		  qptr->enqueue(item);
+  	}
+  	if(qptr->isFull())
+  		cout << "Queue is full\n";
+  	for(int i = 0; i < MAX_QUEUE; i++){
+  		cout << qptr->getFront() << "  ";
+ 	    qptr->dequeue();
+  	}
+  	if(qptr->isEmpty())
+  		cout << "\nQueue is empty\n";
+  	iter ++;
+  	qptr = &pQ;
+	}while(iter < 2);
 }
